a6fe19575f6b47bd0efce91fae60d050814634c6
Nehavinamra <vinamraneha@gmail.com>
2024-02-17 22:54:16 -0500

    del

--- src/loginpage.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const googleSignInButton = document.getElementById("googleSignIn");
-    
-    googleSignInButton.addEventListener("click", function() {
-        window.location.href = "signinwithgoogle.html";
-    });
-});
