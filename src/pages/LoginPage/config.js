a1d1c32c950dfc70b1252464035d99652395ddd9
tanmayee <tatajane@iu.edu>
2024-03-02 10:31:48 -0500

    auth

---
+++ src/pages/LoginPage/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7PntyJJVBjO-Zf4rxoanF4_y4YUmEEH0",
+  authDomain: "deliverease-ccc03.firebaseapp.com",
+  projectId: "deliverease-ccc03",
+  storageBucket: "deliverease-ccc03.appspot.com",
+  messagingSenderId: "132444782556",
+  appId: "1:132444782556:web:291b57ab2027cb515cd919",
+  measurementId: "G-33Q7WKV75Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export{auth,provider};
