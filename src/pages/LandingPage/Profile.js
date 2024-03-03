07d59b1872ab1672bf1bde2902471fe2ad8bae39
brilord <143294125+hellobrian1@users.noreply.github.com>
2024-03-03 10:24:39 -0500

    committing

--- src/pages/LandingPage/Profile.js
+++ src/pages/LandingPage/Profile.js
@@ -1,11 +1,1 @@
-import React from "react";
 
-const Profile = ({ setUserState, username }) => {
-  return (
-    <div className="profile">
-      <h1 style={{ color: "white" }}>Welcome {username} !!</h1>
-      
-    </div>
-  );
-};
-export default Profile;
