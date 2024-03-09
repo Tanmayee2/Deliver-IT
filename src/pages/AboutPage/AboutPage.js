9965fd4ca2645031c2ad90efde17e978b958bbd4
Nehavinamra <vinamraneha@gmail.com>
2024-03-08 19:47:23 -0500

    creating dir aboutpage

---
+++ src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react"; // Import useState from React
+import "./AboutPage.scss";
+
+function AboutPage() {
+  return (
+    <div className="AboutPage">
+      <header>
+        <h1>About Page</h1>
+      </header>
+    </div>
+  );
+}
+export default AboutPage;
