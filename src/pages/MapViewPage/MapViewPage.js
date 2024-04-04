aca77130f02e20a3c40bc2e4d8c3f7d388d62c35
MichealRonan <132925162+MichealRonan@users.noreply.github.com>
2024-04-04 17:36:31 -0500

    UserContext and Navigation added

--- src/pages/MapViewPage/MapViewPage.js
+++ src/pages/MapViewPage/MapViewPage.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import MapComponent from "./MapComponent";
-class MapViewPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: 'Welcome to my basic React page!'
-    };
-  }
-  render() {
+import UserContext from './UserContext';
+
+function MapViewPage() {
+    const [message, setMessage] = useState('Welcome to my basic React page!');
+    const user = useContext(user);
+    const navigate = useNavigate();
+    const goHome = () => navigate('/');
+
     return (
-      <div>
-        <h1>{this.state.message}</h1>
-        <p>This is a very basic React page.</p>
-        <MapComponent/>
-      </div>
+        <div>
+            <h1>{message}</h1>
+            <p>This is a very basic React page.</p>
+            <MapComponent />
+            <div>Welcome, {user.name}!</div>
+            <button onClick={goHome}>Go Home</button>
+        </div>
     );
-  }
 }
 
 export default MapViewPage;
