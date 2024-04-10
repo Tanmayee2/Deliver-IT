4f758c1cd4d76bd6de76fed4cbf17342dc30402e
MichealRonan <132925162+MichealRonan@users.noreply.github.com>
2024-04-10 16:34:45 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,11 +14,16 @@ import AboutPage from './pages/AboutPage/AboutPage';
 import ChatPage from './pages/ChatPage/ChatPage';
 import OrderListPage from './pages/OrderListPage/OrderListPage';
 import DynamicPage from './pages/DynamicPage/DynamicPage';
+<<<<<<< Updated upstream
 import TrackingPage from './pages/TrackingPage/TrackingPage';
+=======
+import { UserProvider } from './UserContext';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <Router>
+<<<<<<< Updated upstream
       <div className="App">
         <Routes>
           <Route path="/Login" element={<LoginPage />} />
@@ -33,6 +38,23 @@ function App() {
           <Route path="/TrackingPage" element={<TrackingPage/>}/>
         </Routes>
       </div>
+=======
+      <UserProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/Login" element={<LoginPage />} />
+            <Route path="/Register" element={<RegisterPage />} />
+            <Route path="/Dashboard" element={<HomePage />} />
+            <Route path="/StatusDelivery" element={<MapViewPage />} />
+            <Route path="/LandingPage" element={<LandingPage />} />
+            <Route path="/About" element={<AboutPage />} />
+            <Route path="/ChatPage" element={<ChatPage />} />
+            <Route path="/OrderListPage" element={<OrderListPage />} />
+            <Route path="/DynamicPage" element={<DynamicPage />} />
+          </Routes>
+        </div>
+      </UserProvider>
+>>>>>>> Stashed changes
     </Router>
   );
 }
