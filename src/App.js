d693f79d1276921d2246a7fb5d1e19b28a80089a
Nehavinamra <vinamraneha@gmail.com>
2024-04-10 21:55:37 -0400

    fixing dimension show/noshow

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-
-import './App.scss';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
-import LoginPage from './pages/LoginPage/LoginPage';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
-import HomePage from './pages/HomePage/HomePage';
-import MapViewPage from './pages/MapViewPage/MapViewPage';
-import LandingPage from './pages/LandingPage/LandingPage';
-import AboutPage from './pages/AboutPage/AboutPage';
-import ChatPage from './pages/ChatPage/ChatPage';
-import OrderListPage from './pages/OrderListPage/OrderListPage';
-import DynamicPage from './pages/DynamicPage/DynamicPage';
-<<<<<<< Updated upstream
-import TrackingPage from './pages/TrackingPage/TrackingPage';
-=======
-import { UserProvider } from './UserContext';
->>>>>>> Stashed changes
+import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import HomePage from "./pages/HomePage/HomePage";
+import MapViewPage from "./pages/MapViewPage/MapViewPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import ChatPage from "./pages/ChatPage/ChatPage";
+import OrderListPage from "./pages/OrderListPage/OrderListPage";
+import DynamicPage from "./pages/DynamicPage/DynamicPage";
+import { UserProvider } from "./UserContext";
 
 function App() {
   return (
     <Router>
-<<<<<<< Updated upstream
-      <div className="App">
-        <Routes>
-          <Route path="/Login" element={<LoginPage />} />
-          <Route path="/Register" element={<RegisterPage/>}/>
-          <Route path="/Dashboard" element={<HomePage/>}/>
-          <Route path="/StatusDelivery" element={<MapViewPage/>}/>
-          <Route path="/LandingPage" element={<LandingPage/>}/>
-          <Route path="/About" element={<AboutPage/>}/>
-          <Route path="/ChatPage" element={<ChatPage/>}/>
-          <Route path="/OrderListPage" element={<OrderListPage/>}/>
-          <Route path="/DynamicPage" element={<DynamicPage/>}/>
-          <Route path="/TrackingPage" element={<TrackingPage/>}/>
-        </Routes>
-      </div>
-=======
       <UserProvider>
         <div className="App">
           <Routes>
@@ -54,7 +29,6 @@ function App() {
           </Routes>
         </div>
       </UserProvider>
->>>>>>> Stashed changes
     </Router>
   );
 }
