d4d6d826977dc06d359418f49f0e7260afa07395
Nehavinamra <vinamraneha@gmail.com>
2024-04-13 00:49:33 -0400

    adding payment context

--- src/App.js
+++ src/App.js
@@ -12,27 +12,66 @@ import MapViewPageManager from "./pages/MapViewPage/MapViewPageManager";
 import SearchPageCustomer from "./pages/SearchPage/SearchPageCustomer";
 import SearchPageManager from "./pages/SearchPage/SearchPageManager";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import PaymentPage from "./pages/PaymentPage/PaymentPage";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
 function App() {
+  const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret: clientSecret,
+    appearance,
+  };
+
   return (
     <UserProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route
-            path="/landingpage/customer"
-            element={<LandingPageCustomer />}
-          />
-          <Route path="/landingpage/driver" element={<LandingPageDriver />} />
-          <Route path="/landingpage/manager" element={<LandingPageManager />} />
-          <Route path="/mapview/customer" element={<MapViewPageCustomer />} />
-          <Route path="/mapview/driver" element={<MapViewPageDriver />} />
-          <Route path="/mapview/manager" element={<MapViewPageManager />} />
-          <Route path="/search/customer" element={<SearchPageCustomer />} />
-          <Route path="/search/manager" element={<SearchPageManager />} />
-          <Route path="/LandingPage" element={<LandingPage />} />
-        </Routes>
-      </Router>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <Router>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route
+                path="/landingpage/customer"
+                element={<LandingPageCustomer />}
+              />
+              <Route
+                path="/landingpage/driver"
+                element={<LandingPageDriver />}
+              />
+              <Route
+                path="/landingpage/manager"
+                element={<LandingPageManager />}
+              />
+              <Route
+                path="/mapview/customer"
+                element={<MapViewPageCustomer />}
+              />
+              <Route path="/mapview/driver" element={<MapViewPageDriver />} />
+              <Route path="/mapview/manager" element={<MapViewPageManager />} />
+              <Route path="/search/customer" element={<SearchPageCustomer />} />
+              <Route path="/search/manager" element={<SearchPageManager />} />
+              <Route path="/LandingPage" element={<LandingPage />} />
+              <Route path="/PaymentPage" element={<PaymentPage />} />
+            </Routes>
+          </Router>
+        </Elements>
+      )}
     </UserProvider>
   );
 }
